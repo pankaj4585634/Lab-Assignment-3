95458716edc7392caa5d00ed7ba344211103586b
Pankaj <pk4585634@gmail.com>
2025-11-26 17:17:46 +0530

    Add Student class with properties and methods

---
+++ exception/threads/model/Student.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Student {
+    private Integer rollNo;  // wrapper class
+    private String name;
+    private String email;
+    private String course;
+    private Double marks;    // wrapper class
+    private char grade;
+
+    public Student(Integer rollNo, String name, String email, String course, Double marks) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+        this.marks = marks;
+        calculateGrade();
+    }
+
+    private void calculateGrade() {
+        if (marks >= 90) grade = 'A';
+        else if (marks >= 75) grade = 'B';
+        else if (marks >= 50) grade = 'C';
+        else grade = 'D';
+    }
+
+    public void display() {
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Course: " + course);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("--------------------------------");
+    }
+
+    public Integer getRollNo() {
+        return rollNo;
+    }
+}
